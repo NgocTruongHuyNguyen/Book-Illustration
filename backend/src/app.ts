ff5daebd685adc7ade891fc7c813ec4b1291bcae
Huy Nguyễn Ngọc Trường <huynguyenngoctruong@Huys-MacBook-Pro.local>
2026-08-15 10:48:44 +1200

    Update: Add cors into backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import { authRouter } from './routes/authRoute.js';
 import { projectsRouter } from './routes/project.js';
 import { requireUser } from './routes/middleware/requireUser.js';
 
 export const app = express();
 
+app.use(cors());
 app.use(express.json({ limit: '5mb' })); 
 app.use('/auth', authRouter);
 app.use('/projects', requireUser, projectsRouter);
