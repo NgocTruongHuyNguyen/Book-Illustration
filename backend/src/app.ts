ebaa60078cb249fb345be13898b544fedfcb0dc9
Huy Nguyễn Ngọc Trường <huynguyenngoctruong@Huys-MacBook-Pro.local>
2026-08-15 11:05:24 +1200

    Update: Add /images route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,11 +3,15 @@ import cors from 'cors';
 import { authRouter } from './routes/authRoute.js';
 import { projectsRouter } from './routes/project.js';
 import { requireUser } from './routes/middleware/requireUser.js';
+import path from 'node:path';
+import { getDataDir } from './storage/config.js';
 
 export const app = express();
 
 app.use(cors());
 app.use(express.json({ limit: '5mb' })); 
+app.use('/images', express.static(path.join(getDataDir(), 'images')));
+app.use('/', authRouter)
 app.use('/auth', authRouter);
 app.use('/projects', requireUser, projectsRouter);
 
